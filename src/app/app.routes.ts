8e8bfa26551c60632c2d33d2ad3780eeff691d1f
Ruan Phablo <ruanphablo778@gmail.com>
2024-12-23 15:48:46 -0300

    Feat: Product page add products!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { HomeComponent } from './views/home/home.component';
+import { ProductsComponent } from './views/products/products.component';
+import { AdminComponent } from './views/admin/admin.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'admin', component: AdminComponent }
 ];
--- src/app/views/products/products.component.ts
+++ src/app/views/products/products.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { ProductService } from '../../core/services/product.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Product } from '../../core/models/product';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products: Product[] = [];
+  images: Image[] = []
 
+  ngOnInit() {
+    this.getAllProducts();
+    this.getAllImageByProducts();
+  }
+
+  constructor(private product_service: ProductService, private sanitizer: DomSanitizer) {
+  }
+
+  getAllProducts() {
+    this.product_service.getProducts().subscribe((data) => {
+      this.products = data;
+    })
+  }
+
+  getAllImageByProducts() {
+    if (this.products != null) {
+      this.products.forEach((product) => {
+        this.product_service.getProductImage(product.product_image_name).subscribe((data) => {
+          let objectURL = URL.createObjectURL(data);
+          this.images.push(new Image(product.product_image_name, this.sanitizer.bypassSecurityTrustUrl(objectURL)))
+        })
+      })
+    }
+  }
+
+  getImageByName(imageName: string) {
+    if (this.images != null) {
+      let image = this.images.find((image) => image.imageName === imageName)
+      return image != null ? image.imageData : null
+    }
+  }
+}
+
+class Image {
+  imageName: string;
+  imageData: any;
+  constructor(imageName: string, imageData: any) {
+    this.imageName = imageName;
+    this.imageData = imageData;
+  }
 }
