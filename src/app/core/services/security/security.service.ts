fedd8e27ecca51c6645c7acf09165f74d4ae5be6
Ruan Phablo <ruanphablo778@gmail.com>
2024-12-26 23:48:56 -0300

    Chore: Create Login and Register empty methods!

--- src/app/core/services/security/security.service.ts
+++ src/app/core/services/security/security.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecurityService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private route: Router) { }
+
+  login(username: string, password: string) {
+
+  }
+
+  register(username: string, password: string) {
+
+  }
 }
