d1359776c4dfed024f22e5172f9185362abc1b3b
Ruan Phablo <ruanphablo778@gmail.com>
2024-12-27 23:21:16 -0300

    Feat: create loginErrorModal!

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, Injectable, PLATFORM_ID, ViewChild } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { SecurityService } from '../../core/services/security/security.service';
 import { User } from '../../core/models/user';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Modal } from 'bootstrap';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
@@ -11,10 +14,9 @@ import { FormsModule, NgForm } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
-
   constructor(private securityService: SecurityService) {
-
   }
 
   login(form: NgForm) {
