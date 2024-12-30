2f419ef023c16d18e98c809954bb15dce73ee24e
Ruan Phablo <ruanphablo778@gmail.com>
2024-12-30 05:33:54 -0300

    Feat: Implement Add And Edit Modal not implement http methods in modal!

--- src/app/views/admin/admin.component.ts
+++ src/app/views/admin/admin.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { Product } from '../../core/models/product';
 import { ProductService } from '../../core/services/product/product.service';
+import { FormsModule, NgForm } from '@angular/forms';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent implements OnInit {
+  @ViewChild('addProductModal') addProductModal!: ElementRef;
+  @ViewChild('editProductModal') editProductModal!: ElementRef;
+  @ViewChild('editProductForm') form!: NgForm;
+
   products: Product[] = []
 
   constructor(private productService: ProductService) {
@@ -29,11 +35,17 @@ export class AdminComponent implements OnInit {
     });
   }
 
-  addProductModal() {
-
+  openAddProductModal() {
+    let modal = new bootstrap.Modal(this.addProductModal.nativeElement)
+    modal.show()
   }
 
-  editProductModal(id: number) {
-
+  openProductEditorModal(product: Product) {
+    let modal = new bootstrap.Modal(this.editProductModal.nativeElement)
+    modal.show()
+    this.form.controls['productName'].setValue(product.product_name)
+    this.form.controls['productDescription'].setValue(product.product_description)
+    this.form.controls['productQuantity'].setValue(product.product_quantity)
+    this.form.controls['productPrice'].setValue(product.product_price)
   }
 }
